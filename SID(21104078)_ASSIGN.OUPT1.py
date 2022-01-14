61120fa7a4dc90605bebccfc32b29f16ab956e73
Gautam2802 <97735608+Gautam2802@users.noreply.github.com>
2022-01-14 21:25:39 +0530

    Add files via upload

---
+++ SID(21104078)_ASSIGN.OUPT1.py
@@ -0,0 +1,35 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:\EE COMPUTING\SID(21104078)_ASSIGNMENT1.py ============
+ENTER NUMBER 1:  98
+ENTER NUMBER 2:  92
+ENTER NUMBER 3:  90
+the average of numbers is:  93.33333333333333
+
+
+Enter your gross income:  900000
+Enter number of dependents:  2
+your annual tax is:   176800.0
+
+
+Enter your name:  Gautam Singal
+Enter your sid:  21104078
+Type M for male,F for female U for unknown
+Enter your gender:  M
+Enter your course name:  INTRODUCTORY TO COMPUTING
+Enter your Cgpa:  10
+['Gautam Singal', 21104078, 'M', 'INTRODUCTORY TO COMPUTING', 10]
+
+
+Enter marks of 1st student: 85
+Enter marks of 2nd student: 96
+Enter marks of 3rd student: 92
+Enter marks of 4th student: 89
+Enter marks of 5th student: 98
+[85, 89, 92, 96, 98]
+
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+New list is: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Answer for part B:  ['Red', 'Green', 'White', 'purple', 'Yellow']
