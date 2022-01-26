f579f023f37b17480c22542b092c22a42291e398
Gautam2802 <97735608+Gautam2802@users.noreply.github.com>
2022-01-26 14:07:35 +0530

    Add files via upload

---
+++ SID(21104078)_ASSIGNMENT.OUTPUT2.py
@@ -0,0 +1,60 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:\EE COMPUTING\SID(21104078)_ASSIGNMENT2.py ============
+
+String Operations
+
+The original String is: "Python is a case sensitive language"
+	length of the given string:  35
+	The reverse order of string is: "egaugnal evitisnes esac a si nohtyP"
+	New String: "a case sensitive"
+	String after the changes: "Python is object oriented language"
+	Index of "a" in the given string: 10
+	String after removing the white spaces: "Pythonisacasesensitivelanguage"
+........................................................................................................................
+
+String Formatting
+
+Enter your Name: Gautam Singal
+Enter your SID: 21104078
+Enter the name of your department: Electrical Engineering
+Enter your CGPA: 10
+
+Hey Gautam Singal Here!
+My SID is 21104078
+I am from Electrical Engineering department and my CGPA is 10.0.
+........................................................................................................................
+
+Bitwise Operators
+
+a: 56
+b: 10
+
+a & b:  8
+a | b:  58
+a ^ b:  50
+Left shifting both a and b with 2 bits: a=224,b=40
+Right shifting a with 2 and b with 4 bits: a=14,b=0
+........................................................................................................................
+
+Greatest Number Calculator
+
+Enter your first number: 98
+Enter your second number: 94
+Enter your third number: 97
+Greatest of the three numbers entered: 98.0
+........................................................................................................................
+
+String Comparison
+
+Enter the string: rahul
+No
+........................................................................................................................
+
+Triangle Validity Checker
+
+Enter the Length of first side: 25
+Enter the Length of second side: 15
+Enter the Length of third side: 37
+Yes, Triangle Can Be formed.
