805ce56284eb887787f209586646ee8bfeb5bf82
Gautam2802 <97735608+Gautam2802@users.noreply.github.com>
2022-02-14 23:05:46 +0530

    Add files via upload

---
+++ SID(21104078)_ASSIGNMENT.OUTPUT3.py
@@ -0,0 +1,87 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:\EE COMPUTING\SID(21104078)_ASSIGNMENT3.py ============
+Enter the String:My branch is electrical engineering
+
+Empty String Has been Entered
+Occurrences of my in "My branch is electrical engineering": 1
+
+Occurrences of electrical in "My branch is electrical engineering": 1
+
+Occurrences of engineering in "My branch is electrical engineering": 1
+
+Occurrences of is in "My branch is electrical engineering": 1
+
+Occurrences of branch in "My branch is electrical engineering": 1
+
+....................................................................................................
+Enter the Day: 28
+Enter the Month: 02
+Enter the Year: 1999
+
+The next Date is : 1/3/1999
+....................................................................................................
+Enter the list of numbers(Enter the numbers separated by space):3 9 10
+
+[(3, 9), (9, 81), (10, 100)]
+....................................................................................................
+Enter the Grade Point: 9
+	Your letter Grade is: A
+	Your performance is: Excellent
+....................................................................................................
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+....................................................................................................
+Enter the name of the student: GAUTAM SINGAL
+Enter the SID the student: 21104078
+ Do you want to enter more details press y/n: y
+Enter the name of the student: VISHNU BANSAL
+Enter the SID the student: 21104042
+ Do you want to enter more details press y/n: y
+Enter the name of the student: RISHAV JINDAL
+Enter the SID the student: 21104095
+ Do you want to enter more details press y/n: n
+	 The Students Details Are:- 
+	 The SID of GAUTAM SINGAL is 21104078:
+	 The SID of VISHNU BANSAL is 21104042:
+	 The SID of RISHAV JINDAL is 21104095:
+--------------------------------------------------
+(b): Sorting  Details Using Student Names
+ The SID of GAUTAM SINGAL is 21104078 :
+ The SID of RISHAV JINDAL is 21104095 :
+ The SID of VISHNU BANSAL is 21104042 :
+--------------------------------------------------
+(c): Sorting Details Using SID: 
+ The SID of VISHNU BANSAL is 21104042 :
+ The SID of GAUTAM SINGAL is 21104078 :
+ The SID of RISHAV JINDAL is 21104095 :
+--------------------------------------------------
+(d): Search a student details with SID and print name: 
+ Enter the SID of student whose details you want to search: 21104078
+ The name of the student whose SID is 21104078 is GAUTAM SINGAL
+....................................................................................................
+Number of fibonacci terms to be printed: 11
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+....................................................................................................
+A =  {1, 3, 5, 6, 8}
+B =  {3, 6, 8, 9, 13, 17}
+C =  {1, 2, 4, 5}
+D =  {6, 7, 8, 9, 10}
+E =  {10, 7}
+....................................................................................................
