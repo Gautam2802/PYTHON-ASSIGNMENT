660890725bf1c6748669ee36475fcc02977d2f91
Gautam2802 <97735608+Gautam2802@users.noreply.github.com>
2022-03-01 22:20:02 +0530

    Add files via upload

---
+++ SID(21104078)_ASSIGNMENT.OUTPUT4.py
@@ -0,0 +1,91 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: D:\EE COMPUTING\SID(21104078)_ASSIGNMENT4.py ============
+Number of discs: 4
+
+A is the prime rod
+B is the auxilliary rod
+C is the terminal rod
+
+Steps:
+Move disk 1 from rod A to rod B
+Move disk 2 from rod A to rod C
+Move disk 1 from rod B to rod C
+Move disk 3 from rod A to rod B
+Move disk 1 from rod C to rod A
+Move disk 2 from rod C to rod B
+Move disk 1 from rod A to rod B
+Move disk 4 from rod A to rod C
+Move disk 1 from rod B to rod C
+Move disk 2 from rod B to rod A
+Move disk 1 from rod C to rod A
+Move disk 3 from rod B to rod C
+Move disk 1 from rod A to rod B
+Move disk 2 from rod A to rod C
+Move disk 1 from rod B to rod C
+
+Number of steps will be: 15
+....................................................................................................
+Enter the number of rows you want to be printed: 6
+The pascal triangle with recursion is: 
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+1 5 10 10 5 1 
+--------------------------------------------------
+Enter the number of rows you want to be printed: 6
+                1                   
+              1   1                 
+            1   2   1               
+          1   3   3   1             
+        1   4   6   4   1           
+     1   5   10   10   5   1        
+....................................................................................................
+Enter the first Integer: 11
+Enter the second Integer: 85
+Quotient:  0
+Remainder:  11
+--------------------------------------------------
+The function is Callable
+--------------------------------------------------
+All the values are not non-zero.
+--------------------------------------------------
+After adding 4,5,6 :  [0, 11, 4, 5, 6]
+Values greater than 4 are :  [11, 5, 6]
+--------------------------------------------------
+Converted set:  {0, 4, 5, 6, 11}
+--------------------------------------------------
+IMMUTABLE SET:   frozenset({0, 4, 5, 6, 11})
+--------------------------------------------------
+Maximum value:  11
+integer hash value of Max value:  11
+....................................................................................................
+NAME =Gautam Singal
+ROLLNO = 21104078
+....................................................................................................
+
+DETAILED ACCOUNTS OF EMPLOYEES
+Employee	Name		Salary
+--------------------------------------------------
+1		Mehak		40000
+2		Ashok		50000
+3		Viren		60000
+--------------------------------------------------
+
+The Updated salary of Mehak is,  70000
+--------------------------------------------------
+
+DETAILED ACCOUNTS OF EMPLOYEES AFTER UPDATION
+Employee	Name		Salary
+--------------------------------------------------
+1		Mehak		40000
+2		Ashok		50000
+....................................................................................................
+Enter the word utter by George: storage
+Enter the word utter by Barbie: ragesto
+
+CONGRATULATIONS! Their Friendship is true.
+....................................................................................................
